2e413440db3f2704aa4ce7778bffd98a629fc09c
NovikovM <novikov1807@gmail.com>
2022-07-04 19:42:01 +0300

    done v.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react';
 import './App.css';
+import Chart from './components/Chart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SPENT TIME</h1>
+      <Chart />
     </div>
   );
 }
---
+++ src/components/Chart.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  BarElement,
+  PointElement,
+  ArcElement,
+  RadialLinearScale,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  BarElement,
+  PointElement,
+  ArcElement,
+  RadialLinearScale,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function Chart() {
+  const [labels, setLabels] = useState(['Landing Page', 'Configurator', 'Check-out', 'Deal']);
+  const [data, setData] = useState([7.4, 0.2, 7, 3.8]);
+  const [chartData, setChartData] = useState({
+    labels: labels,
+    datasets: [
+      {
+        label: "SPENT TIME(SECOND)",
+        data: data,
+        borderColor: ['rgba(54, 162, 235, 0.2)'],
+        backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+      },
+    ],
+  });
+  const [chartOptions, setChartOptions] = useState({});
+ 
+  useEffect(() => {
+    setChartData({
+      labels: labels,
+      datasets: [
+        {
+          label: "(SECOND)",
+          data: data,
+          borderColor: ['rgba(54, 162, 235, 0.2)'],
+          backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+        },
+      ],
+    });
+    setChartOptions({
+      indexAxis: 'y',
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+      },
+    });
+  }, [data]);
+
+  function setRandomData() {
+    console.log('set');
+    setData(data.map(el => el = 60 * Math.random()))
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      setRandomData()
+    }, 30000);
+  }, []);
+
+  return (
+    <div className="chart">
+      <Bar options={chartOptions} data={chartData} />
+      <button className='btn' onClick={setRandomData}>RANDOMIZE</button>
+    </div>
+  );
+}
+
+export default Chart;
